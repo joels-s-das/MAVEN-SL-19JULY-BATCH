db50efbe75358e0b76cd29bfe702a7f95ff0ffcc
Your Name~ <you@example.com>
2025-08-27 21:49:09 +0530

    FirstvProject

---
+++ src/main/java/mypkg/myCalc.java
@@ -0,0 +1,16 @@
+package mypkg;
+
+public class myCalc {
+	int sum(int a, int b) { return a+b; }
+	int diff(int a, int b) { return a-b; }
+	int mul(int a, int b) { return a*b; }
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		myCalc calc = new myCalc();
+		System.out.println("  Sum  is "+ calc.sum(30, 20));
+		System.out.println("  Diff  is "+ calc.diff(30, 20));
+		System.out.println("  Mul  is "+ calc.mul(30, 20));
+	}
+
+}
