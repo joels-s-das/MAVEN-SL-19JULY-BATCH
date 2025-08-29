775bb3237052a0c6eca70d98c390e1a7512995b6
Your Name~ <you@example.com>
2025-08-29 17:22:29 +0530

    reminder is added()

--- src/main/java/mypkg/myCalc.java
+++ src/main/java/mypkg/myCalc.java
@@ -5,6 +5,7 @@ public class myCalc {
 	int diff(int a, int b) { return a-b; }
 	int mul(int a, int b) { return a*b; }
 	int div(int a, int b) { return a/b; }
+	int rem(int a, int b) { return a%b; }
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
@@ -12,7 +13,8 @@ public class myCalc {
 		System.out.println("  Sum  is "+ calc.sum(30, 20));
 		System.out.println("  Diff  is "+ calc.diff(30, 20));
 		System.out.println("  Mul  is "+ calc.mul(30, 20));
-		System.out.println("  Mul  is "+ calc.div(40, 20));
+		System.out.println("  Div  is "+ calc.div(40, 20));
+		System.out.println("  Rem  is "+ calc.rem(40, 20));
 	}
 
 }
