1b118d9949300ed05bc29ef04b0a47175ceb3974
Your Name~ <you@example.com>
2025-09-07 09:14:28 +0530

    prod branch

--- src/main/java/mypkg/myCalc.java
+++ src/main/java/mypkg/myCalc.java
@@ -16,7 +16,7 @@ public class myCalc {
         return Math.sqrt(variance); // square root of variance
     }
 	public void branch() {
-		ystem.out.println("Hello from dev branch ");
+		ystem.out.println("Hello from prod branch ");
 		
 	}
 
