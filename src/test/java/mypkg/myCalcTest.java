50e083f6b8e1f2303d7657d40f38ea063a73beaa
Your Name~ <you@example.com>
2025-09-07 08:04:14 +0530

    main branch

---
+++ src/test/java/mypkg/myCalcTest.java
@@ -0,0 +1,17 @@
+package mypkg;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class myCalcTest {
+	myCalc calc = new myCalc();
+
+	@Test
+	void test() {
+		assertEquals(70,calc.sum(50,20));
+		assertEquals(30,calc.diff(50,20));
+		
+	}
+
+}
